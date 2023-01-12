942fb3cddd292aae4ded30271112b4d6f688e11e
EmenikeIsGreat <emenikeani3@gmail.com>
2023-01-12 02:00:29 -0500

    testing

--- testingWhisper.py
+++ testingWhisper.py
@@ -1,6 +1,6 @@
 import whisper
 
 model = whisper.load_model('base')
-result = model.transcribe('sample1.m4a',fp16 = False)
+result = model.transcribe('test.m4a',fp16 = False)
 
 print(result['text'])
